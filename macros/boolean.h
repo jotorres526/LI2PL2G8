3e37cae295864d0f10c61d975a68104c2567a790
jotorres526 <joaotorres526@gmail.com>
2020-03-09 16:09:20 +0000

    Starting folder structure Added some starting macros

---
+++ macros/boolean.h
@@ -0,0 +1,7 @@
+#ifndef ___BOOLEAN_H___
+#define ___BOOLEAN_H___
+    
+    #define TRUE 1
+    #define FALSE 0
+
+#endif
---
+++ macros/cycle.h
@@ -0,0 +1,7 @@
+#ifndef ___CYCLE_H___
+#define ___CYCLE_H___
+
+    #define FORI(n) for(int i = 0; i < n; i++)
+    #define FORJ(n) for(int j = 0; j < n; j++)
+
+#endif
