25e4bd9e39c05c3ff9d9c9dd7f79c638bb43f7ac
Tatsuya26 <a93261@alunos.uminho.pt>
2020-03-10 19:10:50 +0000

    Função jogar incompleta. Em falta : atualizar o estado com a nova posição da peça e mudar de jogador.

---
+++ controller.c
@@ -0,0 +1,20 @@
+#include "controller.h"
+
+
+/* Função deverá receber o estado atual e uma coordenada e  modificaro estado ao jogar na casa correta
+se a jogada for válida. 
+A função devolve verdadeiro (valor diferente de zero) se for possível jogar e falso (zero) caso
+não seja possível.*/
+
+int jogar(ESTADO *e, COORDENADA c) {
+    if ( obter_estado_casa (e,c)== VAZIO && (c.coluna < 8 && c.coluna > 0) && (c.linha < 8 && c.linha > 0)) {
+        printf("jogar %d %d\n", c.coluna, c.linha);
+        return 1;
+    } else {
+        printf("Jogada Inválida.");
+        return 0;
+    }
+}
+int main () {
+    return 0 ;
+}
