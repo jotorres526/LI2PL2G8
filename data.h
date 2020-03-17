b0822588a3d3d47ec6ee4ec35f0a13c65a443be5
Tatsuya26 <a93261@alunos.uminho.pt>
2020-03-17 15:08:09 +0000

    Documentação da base de dados. Alteração de alguns tipos.

--- data.h
+++ data.h
@@ -1,3 +1,8 @@
+/**
+@file dados.h
+Definição do estado e das funções que o manipulam
+*/
+
 #ifndef ___DATA_H___
 #define ___DATA_H___
 
@@ -7,26 +12,66 @@
 #include "macros/cycle.h"
 #include "macros/boolean.h"
 
-typedef enum {VAZIO, BRANCA, PRETA} CASA;
+/**
+\brief Tipo de dados para os erros
+*/
+typedef enum {
+  OK, 
+  COORDENADA_INVALIDA, 
+  JOGADA_INVALIDA, 
+  ERRO_LER_TAB,
+  ERRO_ABRIR_FICHEIRO,
+} ERROS;
 
+/**
+\brief Tipo de dados para a casa
+*/
+typedef enum {
+  UM = '1',
+  DOIS = '2',
+  VAZIO = '.',
+  BRANCA = '*',
+  PRETA = '#'
+} CASA;
+
+/**
+\brief Tipo de dados para as coordenadas
+*/
 typedef struct {
     int coluna;
     int linha;
 } COORDENADA;
 
+/**
+\brief Tipo de dados para a jogada
+*/
 typedef struct {
     COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
 
+/**
+\brief Tipo de dados para as jogadas
+*/
 typedef JOGADA JOGADAS[32];
 
+/**
+\brief Tipo de dados para o estado
+*/
+
 typedef struct {
+  /** O tabuleiro */
     CASA tab[8][8];
-    COORDENADA ultima_jogada; 
+  /** As jogadas */
     JOGADAS jogadas;
+  /** O número das jogadas, usado no prompt */
     int num_jogadas;
+  /** O jogador atual */
     int jogador_atual;
+  /** O nº de comando, usado no prompt */
+  int num_comando;
+  /** A coordenada da última jogada */
+  COORDENADA ultima_jogada;
 } ESTADO;
 
 //Cria um estado vazio com o tabuleiro inicializado
