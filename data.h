682a6c61cdf8cba3f255135bb96cf61dcd1ad171
jotorres526 <jotorres526@gmail.com>
2020-03-19 12:37:57 +0000

    Fixed data.h to support new functions

--- data.h
+++ data.h
@@ -74,30 +74,26 @@ typedef struct {
   COORDENADA ultima_jogada;
 } ESTADO;
 
-//Cria um estado vazio com o tabuleiro inicializado
-ESTADO *inicializar_estado();
-//Retorna qual o numero do jogador atual(1 ou 2)
-int obter_jogador_atual(ESTADO *estado);
-//Retorna o numero de jogadas efetuadas(Cada jogada tem o movimento dos dois jogadores)
-int obter_numero_de_jogadas(ESTADO *estado);
-//Retorna o estado atual da casa na coordenada c (Branca, Preta ou Vazia)
-CASA obter_estado_casa(ESTADO *e, COORDENADA c);
-//Retorna uma coordenada
+/*******************Manipulação de COORDENADA******************/
 COORDENADA setCoordenada(int line, int col);
-//Função obter a ultima jogada 
-COORDENADA obter_ultima_jogada (ESTADO *e);
-//Retorna uma jogada 
+int getColuna(COORDENADA c);
+int getLinha(COORDENADA c);
+
+/*******************Manipulação de JOGADA******************/
+JOGADA getJogada(ESTADO *e, int idx);
 JOGADA setJogada(COORDENADA c1, COORDENADA c2);
-//Função que adiciona uma jogada ao estado
-void addToJogadas(ESTADO *e, JOGADA j);
-// Função que coloca a o peça numa posição
-void setPosicao(ESTADO *e, COORDENADA cord,  CASA casa);
-// Função que coloca ultima jogada
+COORDENADA getUltimaJogada (ESTADO *e);
 void setUltimaJogada(ESTADO *e, COORDENADA c1);
-//Altera o jogador atual
+void addToJogadas(ESTADO *e, JOGADA j);
+int getNumJogadas(ESTADO *e);
+
+
+/*******************Manipulação de CASA******************/
+CASA getCasa(ESTADO *e, COORDENADA c);
+void setCasa(ESTADO *e, COORDENADA c, CASA casa);
+
+/*******************Manipulação de jogador******************/
 void swapJogador(ESTADO *e);
-// Função que verifica se uma jogada é válida, isto é, se a proxima jogada a ser realizada é vizinha da posição anterior
-int jogada_valida (ESTADO *e, COORDENADA c) ;
-// Função que determina o fim do jogo acaba
-int endGame (ESTADO *e) ;
+/*******************Inicializacao******************/
+ESTADO *inicializar_estado();
 #endif
