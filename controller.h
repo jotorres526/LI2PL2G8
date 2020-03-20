e52e7cd6cd7003bda159951162a1215431b26d78
Tatsuya26 <a93261@alunos.uminho.pt>
2020-03-20 13:06:16 +0000

    ola

--- controller.h
+++ controller.h
@@ -7,7 +7,7 @@ int jogar(ESTADO *estado, COORDENADA c);
 // Função que determina o fim do jogo
 int endGame (ESTADO *e);
 // Função que verifica se uma jogada é válida, isto é, se a proxima jogada a ser realizada é vizinha da posição anterior
-int jogada_valida (ESTADO *e, COORDENADA c);
+int jogada_vizinha (ESTADO *e, COORDENADA c);
 // Funçao que ve qual o char de uma determinada casa.
 char revelar_casa(ESTADO *e, int linha, int coluna);
 //Função que transforma um tabuleiro num array de strings
