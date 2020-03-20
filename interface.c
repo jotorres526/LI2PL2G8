73a64653f7f2842a1cd0f436d0234657619d14b2
Tatsuya26 <a93261@alunos.uminho.pt>
2020-03-20 16:06:07 +0000

    olah

--- interface.c
+++ interface.c
@@ -3,7 +3,7 @@
 
 void mostrar_tabuleiro(ESTADO *e) {
     char **tabuleiro = malloc(8 * sizeof(char *));
-    int size = tab_to_string(tabuleiro, e);
+    int size = tabuleiroToString(tabuleiro, e);
     FORI(8) {
         printf("%d ", 8 - i);
         FORJ(9) putchar(tabuleiro[i][j]);
@@ -14,22 +14,26 @@ void mostrar_tabuleiro(ESTADO *e) {
 }
 
 int interpretador(ESTADO *e) {
+    Boolean over = False;
     char linha[BUF_SIZE], filename[BUF_SIZE];
     char col[2], lin[2];
     FILE *fp;
     printf("Bem vindo ao Rastros!\nInstruções: Q - Sair; gr - Gravar,\n");
     mostrar_tabuleiro(e);
-    for(int i = 0; endGame(e); i++) {
+    for(int i = 0; !over; i++) {
         printf("(#%d Jogada %d Player %d)> ", i, getNumJogadas(e), getjogador(e));
         if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0; //le o comando e retorna 0 caso nao seja escrito nada
         if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) { //If que executa a jogada
             COORDENADA coord = {*col - 'a', *lin - '1'};
-            if(jogar(e, coord)) mostrar_tabuleiro(e);
+            if(jogar(e, coord)) {
+                mostrar_tabuleiro(e);
+                if((over = isTerminado(e))) printf("Game over!\n");
+            }
         } else if(strcmp(linha, "Q\n") == 0) { //Comando para sair do jogo
             printf("Saindo do jogo...\n");
             return 0;
         } else if(sscanf(linha, "gr %s", filename) == 1) { //Comando que guarda o tabuleiro atual (cria um "savefile")
-            gravar(fp, e, filename);
+            if(gravar(fp, e, filename) == OK) printf("Guardado com sucesso\n");
         } else if(sscanf(linha, "ler %s", filename) == 1) { //Comando que le um "savefile"
             fp = fopen(filename, "r");
             fclose(fp);
