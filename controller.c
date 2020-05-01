f868b1916974b01215411fc6cdf43a5b1d51eeda
jotorres526 <jotorres526@gmail.com>
2020-05-01 16:38:26 +0100

    Fixed ler tabuleiro

--- controller.c
+++ controller.c
@@ -52,7 +52,7 @@ Boolean isJogadaValida(ESTADO *e, COORDENADA c) {
 }
 
 void addMovJogador(ESTADO *e, COORDENADA c) {
-    int position = getPointerJogada(e) - 1;
+    int position = getPointerJogada(e);
     int jogador = getjogador(e);
     JOGADA j = (jogador == 1) ? createJogada(c, createNullCoord()) : createJogada(getUltimaJogada(e), c);
     editJogadas(e, j, position);
@@ -123,38 +123,34 @@ ERROS gravar(ESTADO *e, char *filename) {
 
 ERROS ler(ESTADO *e,  char *filename) {
     FILE *fp = fopen(filename, "r");
-    char buffer[64];
+    COORDENADA c1, c2;
+    char buffer[64], jog1[32], jog2[32];
+    int jogada;
     if(fp == NULL) return ERRO_ABRIR_FICHEIRO;
     //Passar o tabuleiro no ficheiro para o estado
     REVERSE_FORI(8){
         fgets(buffer, 64, fp);
-        FROMJ_TO_N(2,10) setCasa(e, setCoordenada(i, j - 2), buffer[j]);
+        FORJ(8) setCasa(e, setCoordenada(i, j), buffer[j]);
     }
-    fgets(buffer, 64, fp);//consumir os caracteres de 'flavour' do tabuleiro
-    fgetc(fp); //Consumir o new line
-    //Passar as jogadas para o estado
-    char jog1[32], jog2[32];
-    int jogada;
-    COORDENADA c1, c2;
+    fgetc(fp);
     resetEstado(e);
     while(fgets(buffer, 64, fp)) {
         //Partir a linha de jogadas em pedaços 
-        int scanned = sscanf(buffer, "Jog %02d: %s %s\n", &jogada, jog1, jog2);
+        int scanned = sscanf(buffer, "%02d: %s %s", &jogada, jog1, jog2);
         c1 = setCoordenada(jog1[1] - '1', jog1[0] - 'a');
-        if(scanned == 3) c2 = setCoordenada(jog2[1] - '1', jog2[0] - 'a');
-        else c2 = createNullCoord();
+        c2 = (scanned == 3) ? setCoordenada(jog2[1] - '1', jog2[0] - 'a') : createNullCoord();
         addToJogadas(e, createJogada(c1, c2));
-        incNumJogadas(e);
-        incPointerJogada(e);
+        if(!isNullCoord(c2)) {
+            incNumJogadas(e);
+            incPointerJogada(e);
+            setUltimaJogada(e, c2);
+            setJogador(e, 1);
+        }
+        else {
+            setUltimaJogada(e, c1);
+            setJogador(e, 2);
+        }   
     }
-    //Alterar a ultima jogada para refletir o 'reloading' da lista de jogadas
-    if (isNullCoord(c2)) {
-        setUltimaJogada(e, c1);
-        setJogador(e, 2);
-    } else {
-        setUltimaJogada(e, c2);
-        setJogador(e, 1);
-    } 
     fclose(fp);
     return OK;
 }
